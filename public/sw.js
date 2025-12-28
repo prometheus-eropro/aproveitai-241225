360cf31886c3bc11a3a7d0290d3af368a8b58592
prometheus-eropro <assistentes.prometheus@gmail.com>
2025-12-27 22:23:44 -0300

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,16 @@
-const CACHE_NAME = "aproveitai-cache-v1";
+const CACHE_NAME = "aproveitai-cache-v2";
 const FILES_TO_CACHE = [
-  "index.html",
-  "manifest.json",
-  "logo-aproveitai.png",
-  "logo-prometheus.png",
-  "painelcliente.html",
-  "beneficios.html",
-  "promocoes.html",
-  "depoimentos.html",
-  "cartao.html"
+  "/index.html",
+  "/style.css",
+  "/manifest.json",
+  "/favicon.ico",
+  "/logo-aproveitai.png",
+  "/logo-prometheus.png",
+  "/painelcliente.html",
+  "/beneficios.html",
+  "/promocoes.html",
+  "/depoimentos.html",
+  "/cartao.html"
 ];
 
 // Instala e guarda cache
